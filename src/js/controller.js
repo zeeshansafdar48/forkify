3da7ba757afb8c4525b8ae6de330b9c9ebec8a29
Zeeshan Safdar <44399893+zeeshansafdar48@users.noreply.github.com>
2021-08-28 10:51:16 +0500

    Make test api call to forkify

--- src/js/controller.js
+++ src/js/controller.js
@@ -11,3 +11,33 @@ const timeout = function (s) {
 // https://forkify-api.herokuapp.com/v2
 
 ///////////////////////////////////////
+
+const showRecipe = async () => {
+  try {
+    const res = await fetch('https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bc886');
+    const data = await res.json();
+    if (!res.ok) {
+      throw new Error(`${data.message}`)
+    }
+
+    let {recipe} = data.data;
+    recipe = {
+      id: recipe.id,
+      imageUrl: recipe.image_url,
+      ingredients: recipe.ingredients,
+      publisher: recipe.publisher,
+      servings: recipe.servings,
+      title: recipe.title,
+      sourceUrl: recipe.source_url,
+      cookingTime: recipe.cooking_time
+    };
+    console.log('Recipe', recipe)
+
+  } catch (error) {
+    console.log('error ==>', error)
+    alert(error);
+  }
+
+}
+
+showRecipe();
